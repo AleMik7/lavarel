2335a9e7de16a2985a7868c1f1a0fef74b7d0736
Alesia <terfukl@gmail.com>
2025-12-17 16:02:57 +0100

    Create databaseseeders.php

---
+++ databaseseeders.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AdminUser;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+     //   User::factory()->create([
+       //     'name' => 'Test User',
+      //      'email' => 'test@example.com',
+       // ]);
+        \App\Models\User::factory(10)->create();
+
+        Post::factory(10)->create();
+        AdminUser::factory(1)->create([
+            "name"=>"Admin",
+            "email"=>"laravel@laravel.com",
+            "password"=> bcrypt(value:"12345"),
+        ]);
+
+    }
+}
